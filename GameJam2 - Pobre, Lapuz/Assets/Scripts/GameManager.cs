451aaf8ba921d432c23347e2d47762e96085e071
Mathieu Marc Pobre <mathieu_marc_pobre@dlsu.edu.ph>
2024-07-11 00:08:38 +0800

    Looping is now Seamless

--- GameJam2 - Pobre, Lapuz/Assets/Scripts/GameManager.cs
+++ GameJam2 - Pobre, Lapuz/Assets/Scripts/GameManager.cs
@@ -6,10 +6,11 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
-    public Transform OriginPoint;
-    public Transform Entrance;
-    public Transform EntranceTarget;
-    public Transform OriginTarget;
+    public Transform TestPoint;
+    public Transform TestPoint2;
+    public Transform TestTarget;
+    public Transform TestTarget2;
+
     public GameObject Player;
 
 
@@ -28,30 +29,21 @@ public class GameManager : MonoBehaviour
 
     private void OnLoop(Parameters Portal)
     {
-        
-        if (OriginPoint.name == Portal.GetStringExtra("Portal", "Cube"))
-        {
-            Debug.Log("ToEntrance");
-            Debug.Log(Player.transform.position.x + "," + Player.transform.position.y + "," + Player.transform.position.z);
-            Vector3 offsetPosition = Player.transform.position - OriginPoint.position;
-            //Vector3 point = new Vector3(Entrance.position.x + 6.5f + offsetPosition.x, 1, Entrance.position.z + offsetPosition.z);
-            //Vector3 point = new Vector3(EntranceTarget.position.x + offsetPosition.x - 0.5f, 1, EntranceTarget.position.z + offsetPosition.z );
-            Vector3 newPos = EntranceTarget.position + offsetPosition;
+       
+        if(TestPoint.name == Portal.GetStringExtra("Portal", "Test")){
+            Vector3 offsetPosition = Player.transform.position - TestPoint.position;
+            Vector3 newPos = TestTarget.position + offsetPosition;
             newPos.y = 1f;
             Player.transform.position = newPos;
             Physics.SyncTransforms();
-            Debug.Log(Player.transform.position.x + "," + Player.transform.position.y + "," + Player.transform.position.z);
-
         }
-        else if(Entrance.name == Portal.GetStringExtra("Portal", "Cube (1)"))
+        else if (TestPoint2.name == Portal.GetStringExtra("Portal", "Test2"))
         {
-            Debug.Log("ToOrigin");
-             Debug.Log(Player.transform.position.x + "," + Player.transform.position.y + "," + Player.transform.position.z);
-            Vector3 offsetPosition = Player.transform.position - Entrance.position;
-            Vector3 point = new Vector3(OriginPoint.position.x - 5.9f - offsetPosition.x, 1, OriginPoint.position.z + offsetPosition.z);
-            Player.transform.position = point;
+            Vector3 offsetPosition = Player.transform.position - TestPoint2.position;
+            Vector3 newPos = TestTarget2.position + offsetPosition;
+            newPos.y = 1f;
+            Player.transform.position = newPos;
             Physics.SyncTransforms();
-
         }
     }
 
